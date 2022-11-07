2127434fc829f651f4af57b680311b06eb8b1ca7
TheBlackFenix <danielh1991@hotmail.com>
2022-11-07 11:59:37 -0500

    Added Endpoint, Detail of a Transaction

---
+++ AlkemyWallet/Controllers/TransactionController.cs
@@ -0,0 +1,39 @@
+﻿using AlkemyWallet.Core.Interfaces;
+using AlkemyWallet.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlkemyWallet.Controllers
+{
+    [Route("[Controller]")]
+    [ApiController]
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionController(ITransactionService transaction)
+        {
+            _transactionService = transaction;
+        }
+
+        [HttpGet("{id")]
+        [Authorize]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var transactionDetail = await _transactionService.getById(id);
+                if (transactionDetail is null)
+                {
+                    return NotFound();
+                }
+                return Ok(transactionDetail);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Status = "Bad Request", Message = $"Bad Request: {ex.Message}" });
+            }
+            
+        }
+        
+    }
+}
